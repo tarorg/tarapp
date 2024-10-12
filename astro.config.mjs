5b6d2c9ca013fe9c350076b8ca901c8a7730f759
wetarorg <tarframework@gmail.com>
2024-10-12 23:27:44 +0530

    tar24.9

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  // other config options...
+  integrations: [tailwind()],
   vite: {
     ssr: {
       noExternal: ['tinybase'],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '70vh': '70vh',
+      },
+    },
   },
   plugins: [],
 }
-
